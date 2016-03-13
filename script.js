c101d6d606a5794d50fbd841756cc403fbd5633a
dargaCode <darga.code@gmail.com>
2016-03-12 19:48:51 -0800

    Add basic functionality for increasing score

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var DEFAULT_MAX = 5;
+
+var scores = {
+  p1: 0,
+  p2: 0,
+  max: DEFAULT_MAX
+};
+
+// SELECTORS
+
 var p1Score = document.querySelector(".p1-score");
 var p2Score = document.querySelector(".p2-score");
 var winScore = document.querySelector(".score-to-win");
@@ -6,18 +16,43 @@ var p2Button = document.querySelector(".p2-btn");
 var resetButton = document.querySelector(".reset-btn");
 var scoreDial = document.querySelector("input");
 
+// EVENTS
+
 p1Button.addEventListener("click", incrementP1);
 p2Button.addEventListener("click", incrementP2);
 resetButton.addEventListener("click", resetScores);
 
 function incrementP1() {
-  alert("p1 scores!")
+  if (scores.p1 < scores.max) {
+    scores.p1++;
+    console.log("p1 scores!");
+  }
+  updateUi();
 }
 
 function incrementP2() {
-  alert("p2 scores!");
+  if (scores.p2 < scores.max) {
+    scores.p2++;
+    console.log("p2 scores!");
+  }
+  updateUi();
 }
 
 function resetScores() {
-  alert("reset scores");
+  scores.p1 = 0;
+  scores.p2 = 0;
+  console.log("reset scores");
+  updateUi();
 }
+
+// LOGIC
+
+function updateUi() {
+  p1Score.innerHTML = scores.p1;
+  p2Score.innerHTML = scores.p2;
+  scoreDial.value = scores.max;
+}
+
+// MAIN
+
+updateUi();
