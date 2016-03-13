8d030d2239634fcc2e1636cb90808b77f3a5afa6
dargaCode <darga.code@gmail.com>
2016-03-12 19:02:08 -0800

    Add selectors for all needed objects

---
+++ script.js
@@ -0,0 +1,7 @@
+var p1Score = document.querySelector(".p1-score");
+var p2Score = document.querySelector(".p2-score");
+var winScore = document.querySelector(".score-to-win");
+var p1Button = document.querySelector(".p1-btn");
+var p2Button = document.querySelector(".p2-btn");
+var resetButton = document.querySelector(".reset-btn");
+var scoreDial = document.querySelector("input");
