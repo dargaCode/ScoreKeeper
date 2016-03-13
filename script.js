f644cde89a89a045ecd38a94c1bdabea8d5cfcc6
dargaCode <darga.code@gmail.com>
2016-03-12 19:07:48 -0800

    Add button event listeners

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ var p1Button = document.querySelector(".p1-btn");
 var p2Button = document.querySelector(".p2-btn");
 var resetButton = document.querySelector(".reset-btn");
 var scoreDial = document.querySelector("input");
+
+p1Button.addEventListener("click", incrementP1);
+p2Button.addEventListener("click", incrementP2);
+resetButton.addEventListener("click", resetScores);
+
+function incrementP1() {
+  alert("p1 scores!")
+}
+
+function incrementP2() {
+  alert("p2 scores!");
+}
+
+function resetScores() {
+  alert("reset scores");
+}
