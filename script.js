a5273788e31dde7ac6152b8d82d4a99869003015
dargaCode <darga.code@gmail.com>
2016-03-13 22:06:09 -0700

    Update target input min in updateUi()

--- script.js
+++ script.js
@@ -112,6 +112,7 @@ function updateUi() {
   p1Display.innerHTML = scores.p1;
   p2Display.innerHTML = scores.p2;
   targetScoreInput.value = scores.target;
+  targetScoreInput.min = getMinTargetScore();
 
   if (isGameOver()) {
     declareWinner();
